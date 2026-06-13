07c294e86d8d1f637224fae6b1dc6e72cf5a9666
mdsalahuddin96 <salauddincse96@gmail.com>
2026-06-13 12:35:05 +0600

    added a new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,8 +3,37 @@ import p2 from "@/assets/project2.png";
 import p3 from "@/assets/project3.png";
 import p4 from "@/assets/project4.png";
 import p5 from "@/assets/project5.png";
-
+import p6 from "@/assets/project6.png";
 export const projects = [
+  {
+    id: 6,
+    title: "PawNest",
+    category: "Full Stack",
+    img: p6,
+    tags: [
+      "React",
+      "Next.js",
+      "Express.js",
+      "MongoDB",
+      "Tailwind",
+      "BetterAuth",
+      "JWT",
+    ],
+    desc: "PawNest is a modern full-stack MERN-based Pet Adoption Platform where users can explore pets, submit adoption requests, manage their listings, and give rescued animals a loving forever home. The platform provides a smooth and responsive user experience with secure authentication and pet management features.",
+    live: "https://paw-nest-client-blush.vercel.app/",
+    code: "https://github.com/mdsalahuddin96/PawNest-client.git",
+    challenges: [
+      "Implementing secure authentication both server and client",
+      "User Dashboard Design",
+      "Managing responsive UI consistency",
+    ],
+
+    future: [
+      "Add admin dashboard",
+      "Add payment system",
+      "Improve search functionality",
+    ],
+  },
   {
     id: 1,
     title: "Luxury Tiles Gallery",
@@ -29,7 +58,7 @@ export const projects = [
   {
     id: 2,
     title: "The Dragon News",
-    category: "Full Stack",
+    category: "Frontend",
     img: p2,
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "BetterAuth"],
     desc: "Dragon News is a modern news web application built with Next.js. Users can explore dynamic news categories, read full articles, and access protected routes through an authentication system. ",
@@ -47,8 +76,12 @@ export const projects = [
     desc: "Keen Keeper is a smart friend relationship tracking web application that helps you maintain meaningful connections by reminding you when to reach out. It allows you to monitor your interactions, stay consistent, and never lose touch with important people.",
     live: "https://keen-keeper-nu-teal.vercel.app/",
     code: "https://github.com/mdsalahuddin96/keen-keeper.git",
-    challenges: ["Store all interaction and Showing in timeline","Filter timeline by different interaction type", "Showing all interaction with charts"],
-    future: ["Dark mode","Add New Friend","Get Notification based on status"],
+    challenges: [
+      "Store all interaction and Showing in timeline",
+      "Filter timeline by different interaction type",
+      "Showing all interaction with charts",
+    ],
+    future: ["Dark mode", "Add New Friend", "Get Notification based on status"],
   },
   {
     id: 4,
@@ -59,8 +92,16 @@ export const projects = [
     desc: "Book Vibe is a modern and responsive web application designed for book lovers. It allows users to explore different books, view detailed information, and manage their reading interests. The goal of this project is to create a clean and user-friendly interface where users can easily discover books and organize their reading journey.",
     live: "https://my-books-vibe.netlify.app/",
     code: "https://github.com/mdsalahuddin96/keen-keeper.git",
-    challenges: ["Handle Different Route", "Managing the logic between the Read List and Wishlist was challenging, as books in the Read List could not be added to the Wishlist, while Wishlist items could still be moved to the Read List without creating duplicate states."],
-    future: ["Dark mode","Add New Book","Delete/Update Book", "User Authentication"],
+    challenges: [
+      "Handle Different Route",
+      "Managing the logic between the Read List and Wishlist was challenging, as books in the Read List could not be added to the Wishlist, while Wishlist items could still be moved to the Read List without creating duplicate states.",
+    ],
+    future: [
+      "Dark mode",
+      "Add New Book",
+      "Delete/Update Book",
+      "User Authentication",
+    ],
   },
   {
     id: 5,
@@ -71,7 +112,11 @@ export const projects = [
     desc: "DigiTools is a modern web-based utility platform that provides multiple useful digital tools in one place. The goal of this project is to make everyday online tasks easier by offering different productivity and utility features through a clean and user-friendly interface.",
     live: "https://salauddin-digitools.netlify.app/",
     code: "https://github.com/mdsalahuddin96/DigiTools.git",
-    challenges: ["Handling Different Route", "Showing cart product count on navbar","Handle Duplicate product in the cart"],
-    future: ["Signin/Signup","Authentication","Payment System"],
+    challenges: [
+      "Handling Different Route",
+      "Showing cart product count on navbar",
+      "Handle Duplicate product in the cart",
+    ],
+    future: ["Signin/Signup", "Authentication", "Payment System"],
   },
 ];
